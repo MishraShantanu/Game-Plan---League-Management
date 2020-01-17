152901701d24b61b489d665d8897268f44b45d72
Brayden M <braydenkaidmartin@gmail.com>
2020-01-17 14:42:22 -0600

    Added test for setEmail().

--- app/src/main/java/testing/UserTesting.java
+++ app/src/main/java/testing/UserTesting.java
@@ -7,7 +7,7 @@ public class UserTesting {
     /**
      * Tests the setName() function from the User class.
      */
-    public static void testSetName() {
+    private static void testSetName() {
         String[] expected = {"a", "b", "JOHN BYER", "123", ""};
 
         // Setting up test cases.
@@ -35,11 +35,45 @@ public class UserTesting {
     }
 
 
+    private static void testSetEmail() {
+        String[] expected = {
+                "tom@mail.com",
+                "123@mail.com",
+                "test@gmail.com",
+                "foo@mail.ca",
+                "bar@mail.ca"};
+
+        // Setting up test cases.
+        User[] testUsers = {
+                new User("a", "a@mail.com", "1"),
+                new User("a", "b@mail.com", "1"),
+                new User("a", "c@mail.com", "1"),
+                new User("a", "d@mail.com", "1"),
+                new User("a", "e@mail.com", "1")
+        };
+
+        // Changing the email of the users.
+        testUsers[0].setEmail("tom@mail.com");
+        testUsers[1].setEmail("123@mail.com");
+        testUsers[2].setEmail("test@gmail.com");
+        testUsers[3].setEmail("foo@mail.ca");
+        testUsers[4].setEmail("bar@mail.ca");
+
+        // Checking to see if setEmail() changed the email of the user.
+        for (int i = 0; i < testUsers.length; i++) {
+            if (!testUsers[i].getEmail().equals(expected[i])) {
+                System.out.println("Error testSetEmail(): Test #" + (i+1) + "\n\tExpected: " +
+                        expected[i] + ", received: " + testUsers[i].getEmail() + "\n");
+            }
+        }
+    }
+
+
     /**
      * Tests the setSetPhoneNumber() function from the User class.
      * (setPhoneNumber() is used inside the User constructor)
      */
-    public static void testSetPhoneNumber() {
+    private static void testSetPhoneNumber() {
         int expected = 1234567890;
         int[] secondExpected = {1, 1234567890, 3, 4, 5};
 
@@ -60,6 +94,7 @@ public class UserTesting {
             }
         }
 
+        // Changing the phone number of the users.
         testUsers[0].setPhoneNumber("1");
         testUsers[1].setPhoneNumber("q1w2e3r4t5y6u7i8o9p0");
         testUsers[2].setPhoneNumber("3");
@@ -81,6 +116,9 @@ public class UserTesting {
 
         System.out.println("-- SetName() --");
         testSetName();
+
+        System.out.println("-- SetEmail() --");
+        testSetEmail();
 
         System.out.println("-- SetPhoneNumber() --");
         testSetPhoneNumber();
