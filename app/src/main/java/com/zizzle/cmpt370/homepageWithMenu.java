c5109147da06e662d28f2dde79b10995056ffc23
Jay Shah <jrs349@mail.usask.ca>
2020-01-18 12:21:06 -0600

    homepageWithMenu Java: transferred code from "homepage Java" and changed Class name and corrected setContentView. homepageWithMenu XML: transferred code from "homepage XML" and changed context String.

---
+++ app/src/main/java/com/zizzle/cmpt370/homepageWithMenu.java
@@ -0,0 +1,40 @@
+package com.zizzle.cmpt370;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class homepageWithMenu extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout; //main layout ID of homepage.xml
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepagewithmenu);
+
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        //four parameters: the activity, drawer layout, open String (see strings.xml in values folder), close String (see strings.xml)
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays menu button
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //allows menu button to show menu on click
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
