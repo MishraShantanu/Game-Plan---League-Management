a00f42ad5998aa9404f29edbd5b8d20806e0f166
Jay Shah <jrs349@mail.usask.ca>
2020-01-17 23:01:49 -0600

    Now side-bar navigation menu button is visible

--- app/src/main/java/com/zizzle/cmpt370/Homepage.java
+++ app/src/main/java/com/zizzle/cmpt370/Homepage.java
@@ -1,4 +1,29 @@
 package com.zizzle.cmpt370;
 
-public class Homepage {
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Homepage extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout; //main layout ID of homepage.xml
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        //four parameters: the activity, drawer layout, open String (see strings.xml in values folder), close String (see strings.xml)
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays menu button
+    }
 }
