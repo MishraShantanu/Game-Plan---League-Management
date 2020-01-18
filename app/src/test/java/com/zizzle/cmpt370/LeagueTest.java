001dbfd264a587de6e0a29849404b9e7faaf5d44
Brayden M <braydenkaidmartin@gmail.com>
2020-01-18 17:12:56 -0600

    Created a testing file for League.

---
+++ app/src/test/java/com/zizzle/cmpt370/LeagueTest.java
@@ -0,0 +1,12 @@
+package com.zizzle.cmpt370;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LeagueTest {
+
+    @Test
+    public void testSetName() {
+        assertEquals(1, 1);
+    }
+}
