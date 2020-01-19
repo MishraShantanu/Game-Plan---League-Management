4298f6c3aaf6c05e84a650fbae589f5c8ae687b5
Lec337 <lec337@mail.usask.ca>
2020-01-19 00:36:34 -0600

    Added instance variables of class

--- app/src/main/java/com/zizzle/cmpt370/Game.java
+++ app/src/main/java/com/zizzle/cmpt370/Game.java
@@ -1,8 +1,25 @@
 package com.zizzle.cmpt370;
 
+import android.util.Pair;
+
+import java.text.SimpleDateFormat;
+
 /**
  * Parent class, stores generic information about sports games
  */
 public class Game {
+
+    /** teams who are in the game */
+    private Pair<Team,Team> teams;
+
+    /** date (year, month, day, time) the game is being/was played */
+    private SimpleDateFormat date;
+
+    /** name of the location the game is/was held at */
+    private String location;
+
+    /** sport of the game */
+    private String sport;
+
 
 }
