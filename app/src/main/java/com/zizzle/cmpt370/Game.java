8774b850ab08e5881d2a6af9241285c8c55d41e3
Lec337 <lec337@mail.usask.ca>
2020-01-19 01:26:31 -0600

    Implemented Game constructor

--- app/src/main/java/com/zizzle/cmpt370/Game.java
+++ app/src/main/java/com/zizzle/cmpt370/Game.java
@@ -2,7 +2,7 @@ package com.zizzle.cmpt370;
 
 import android.util.Pair;
 
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Parent class, stores generic information about sports games
@@ -13,13 +13,39 @@ public class Game {
     private Pair<Team,Team> teams;
 
     /** date (year, month, day, time) the game is being/was played */
-    private SimpleDateFormat date;
+    private Calendar date;
 
     /** name of the location the game is/was held at */
     private String location;
 
-    /** sport of the game */
+    /** name of the sport of the game */
     private String sport;
+
+
+    /**
+     * Game constructor
+     * @param team1: Team object, first of the teams playing in the game
+     * @param team2: Team object, second of the teams playing in the game
+     * @param gameDate: int array of the form [year, month, day, hour, minute], months are from 0-11,
+     *               hour is on a 24 hr clock ie 1-24
+     * @param location: String name of the location the game is being held
+     * @param sport: String name of the sport of the game
+     */
+    public Game(Team team1, Team team2, int[] gameDate, String location, String sport) throws IllegalArgumentException{
+        //TODO: Could have home and away teams, team1 could be home etc
+        this.teams = new Pair<>(team1,team2);
+
+        // check that time fields are valid
+        int month = gameDate[1];
+        if(month < 0 || gameDate[1] > 11){
+            throw new IllegalArgumentException("Game: gameDate month out of bounds, valid bounds: 0-11" +
+                    " actual month: " + month);
+        }
+        date.set(gameDate[0],gameDate[1],gameDate[2], gameDate[3], gameDate[4]);
+        this.location = location;
+        this.sport = sport;
+    }
+
 
 
 }
