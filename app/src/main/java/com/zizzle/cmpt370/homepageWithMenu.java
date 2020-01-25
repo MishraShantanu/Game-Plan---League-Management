138b9bc4a36e0e3d24b69b4a63223d5bce9ff805
Jay Shah <jrs349@mail.usask.ca>
2020-01-24 23:23:00 -0600

    Now using toolbar from mainmenu page instead of nav_action. Added comments. Added function to check for back button press (which now closes menu instead of closing activity)

--- app/src/main/java/com/zizzle/cmpt370/homepageWithMenu.java
+++ app/src/main/java/com/zizzle/cmpt370/homepageWithMenu.java
@@ -1,5 +1,6 @@
 package com.zizzle.cmpt370;
 
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,22 +21,33 @@ public class homepageWithMenu extends AppCompatActivity {
         setContentView(R.layout.homepagewithmenu); //content to open
 
         //Adding overlay effect for menu
-        mToolBar = (Toolbar) findViewById(R.id.nav_action);
-        setSupportActionBar(mToolBar);
+        mToolBar = (Toolbar) findViewById(R.id.toolbar); //////////changed to toolbar from menuMain instead of using nav_action
+        setSupportActionBar(mToolBar); //sets toolbar as action bar
 
         //MENU (button & drawer)
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        //four parameters: the activity, drawer layout, open String (see strings.xml in values folder), close String (see strings.xml)
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-
+        //four parameters: the activity, drawer layout, toolbar, open String (see strings.xml in values folder), close String (see strings.xml)
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolBar,
+                R.string.sidebar_navigation_open, R.string.sidebar_navigation_close); //added "menu button" which automatically animates icon for open/close
         mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        mToggle.syncState(); //takes care of rotating the menu icon
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //displays menu button
     }
 
+    //WHe back button is pressed, we want to just close the menu, not close the activity
     @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) { //If drawer (sidebar navigation) is open, close it. START is because menu is on left side (for right side menu, use "END")
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
     //Button to open menu
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) { //allows menu button to show menu on click
         if (mToggle.onOptionsItemSelected(item)) {
             return true;
