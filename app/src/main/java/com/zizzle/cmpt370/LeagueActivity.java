3cbbcd383fe52f559f675f72ee993129dbc407e1
Brayden M <braydenkaidmartin@gmail.com>
2020-02-06 13:13:38 -0600

    Have the ListView displaying boxes for each League

--- app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
+++ app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
@@ -6,8 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class LeagueActivity extends AppCompatActivity {
+
+    String[] leagues = {"TEST1", "TEST2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,13 @@ public class LeagueActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ArrayAdapter leagueArrayAdapter = new ArrayAdapter<String>(this, R.layout.league_listview, leagues);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        ListView leagueList = findViewById(R.id.leagues_list);
+//        ListView listView = (ListView) findViewById(R.id.mobile_list);
+
+        leagueList.setAdapter(leagueArrayAdapter);
+//        listView.setAdapter(adapter);
     }
 }
