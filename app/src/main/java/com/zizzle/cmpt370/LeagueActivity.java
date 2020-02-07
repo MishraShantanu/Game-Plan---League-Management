368079e16bb74248d6cddf6d0df2d57b9829cb2e
Brayden M <braydenkaidmartin@gmail.com>
2020-02-07 14:56:18 -0600

    Clicking on ListView items will generate a unique output string

--- app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
+++ app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
@@ -1,21 +1,20 @@
 package com.zizzle.cmpt370;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class LeagueActivity extends AppCompatActivity {
 
     // values inside ListView.
-    ArrayList<String> leagues;
+    ArrayList<League> leagues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +23,33 @@ public class LeagueActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // ListView
+
+    // ListView
+        // TESTING. fills the array with example leagues.
         leagues = new ArrayList<>();
-        leagues.add("League 1");
-        leagues.add("League 2");
-        leagues.add("League 3");
-        leagues.add("League 4");
-        leagues.add("League 5");
-        leagues.add("League 6");
-        leagues.add("League 1");
-        leagues.add("League 2");
-        leagues.add("League 3");
-        leagues.add("League 4");
-        leagues.add("League 5");
-        leagues.add("League 6");
+        Member owner = new Member("Tom", "Holland", "e@mail.gov", "1234567890");
+        for (int i = 0; i < 20; i++) {
+            leagues.add(new League("League " + i, owner, "SQUASH", "description"));
+        }
+
+        // TESTING. gathers the league names from the leagues in system.
+        ArrayList<String> league_names = new ArrayList<>();
+        for (League l :leagues) {
+            league_names.add(l.getName());
+        }
 
         // Display ListView contents.
-        ArrayAdapter leagueArrayAdapter = new ArrayAdapter<String>(
-                this, R.layout.league_listview, leagues);
+        ArrayAdapter leagueArrayAdapter = new ArrayAdapter<>(
+                this, R.layout.league_listview, league_names);
         ListView leagueList = findViewById(R.id.leagues_list);
         leagueList.setAdapter(leagueArrayAdapter);
+
+        // clicking on a league.
+        leagueList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(LeagueActivity.this, "You just clicked " + i, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
