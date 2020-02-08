4a6cd097acef1a38beebea31fb3346772d331c69
Jay Shah <jrs349@mail.usask.ca>
2020-02-07 23:55:21 -0600

    LeagueActivity.java: Added onNavigationItemSelection to open new pages when a menu option is clicked. TODO: pages to open fragments don't work, since it uses a fragment container (which doesn't exist in the League Activity XML)

--- app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
+++ app/src/main/java/com/zizzle/cmpt370/LeagueActivity.java
@@ -1,7 +1,10 @@
 package com.zizzle.cmpt370;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +17,11 @@ import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
-public class LeagueActivity extends AppCompatActivity {
+public class LeagueActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     // values inside ListView.
     ArrayList<League> leagues;
@@ -40,7 +45,7 @@ public class LeagueActivity extends AppCompatActivity {
         //MENU (button & drawer)
         mDrawerLayout = (DrawerLayout) findViewById(R.id.league_layout);
         NavigationView navigationView = findViewById(R.id.league_nav_view); //ADDED FOR CLICK
-//        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
 
         //four parameters: the activity (either "this" or getActivity()"), instance of drawer layout, toolbar, open String (see strings.xml in values folder), close String (see strings.xml)
         // ActionBarDrawerToggle sets up the app icon on the left of the top bar to open & close the navigation drawer
@@ -82,13 +87,45 @@ public class LeagueActivity extends AppCompatActivity {
         });
     }
 
+    //When item is selected in the menu, open the respective element (fragment or activity)
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_leagues:
+                Intent intent = new Intent(this, LeagueActivity.class);
+                startActivity(intent);
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new LeaguesFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_aboutUs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AboutUsFragment()).commit();
+                break;
+            case R.id.nav_logOut:
+                FirebaseAuth.getInstance().signOut();
+                Intent tolog = new Intent(this, SigninActivity.class);
+                startActivity(tolog);
+        }
+        //close drawer
+        mDrawerLayout.closeDrawer(GravityCompat.START);
 
-//    //Button to open menu
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) { //allows menu button to show menu on click
-//        if (mToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+        return true;
+    }
+
+    //Button to open menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //allows menu button to show menu on click
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
