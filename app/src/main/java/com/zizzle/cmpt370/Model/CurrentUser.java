2aeb6a63d3c41feea087137e3c54d7e6ef7aadc2
Lec337 <lec337@mail.usask.ca>
2020-02-28 02:04:07 -0600

    Added methods to read and write leagues to database

--- app/src/main/java/com/zizzle/cmpt370/Model/CurrentUser.java
+++ app/src/main/java/com/zizzle/cmpt370/Model/CurrentUser.java
@@ -3,7 +3,7 @@ package com.zizzle.cmpt370.Model;
 /**
  * Uses the singleton pattern to store a Member object with information about the current user of the app
  */
-public class CurrentUser {
+public final class CurrentUser {
     /** current user of the app */
     private static Member currentUser = null;
 
@@ -11,10 +11,15 @@ public class CurrentUser {
         // constructor is private so the class cannot be instantiated
     }
 
+    /**
+     * Returns a Member object filled with information about the current user of the app
+     * @return Member, current user of the app
+     */
     public static Member getCurrentUser(){
         if(currentUser == null){
-            // we don't have any info about the current user, read this from database
-            
+            // we don't have any info about the current user, get info about current user
+            //TODO either read user data from database, or read data from local storage
+
         }
         // currentUser now stores info about the current user of the app
         return currentUser;
--- app/src/main/java/com/zizzle/cmpt370/Model/Storage.java
+++ app/src/main/java/com/zizzle/cmpt370/Model/Storage.java
@@ -1,47 +1,37 @@
 package com.zizzle.cmpt370.Model;
 
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 
 /**
  * Static class used to read/write from database
  */
 public class Storage {
     /** Reference to the database */
-    private DatabaseReference database;
+    private static DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    /** Stores the league read in, we require this to overcome scoping issues */
+    private static League retrievedLeague;
 
     /**
-     * Adds the input league to the database
-     * @param newLeague: League object to be added to the database, the name of this league must be
-     *                 unique
-     * @throws IllegalStateException if the name of the league isn't unique
-     * @throws DatabaseException if accessing the database fails
+     * Reads the League with the input name from the database
+     * @param leagueName: String, name of the database to read from the database
+     * @return League object with the name input or null if no such league exists
+     * @throws DatabaseException if the database read fails
      */
-    public void addLeague(final League newLeague) throws IllegalStateException, DatabaseException{
-        // determine if league name is unique in the database
+    public static League readLeague(String leagueName) throws DatabaseException{
+        // addListenerForSingleValueEvent reads from the database exactly once
 
-        // addListenerForSingleValueEvent will read from the database exactly once when the listener is created
-        // here we try to read a league with the new league's name from the database,
-        // if there is such a league, the new league doesn't have a unique name
-        database.child("Leagues").child(newLeague.getName()).addListenerForSingleValueEvent(new ValueEventListener() {
+        database.child("Leagues").child(leagueName).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                    // a league with the new league's name already exists, cannot create a new league with this name
-                    throw new IllegalStateException("League creation failed; league with name: " + newLeague.getName() + " already exists");
-                }
-                else{
-                    // add new league to the database
-                    database.child("Leagues").child(newLeague.getName()).setValue(newLeague);
-                }
+                // called when data is read from database
+                retrievedLeague = dataSnapshot.getValue(League.class);
             }
 
             @Override
@@ -50,8 +40,35 @@ public class Storage {
                 throw new DatabaseException("Failed to read league information from database: " + databaseError.getMessage());
             }
         });
+        // return the league read from the database, may be null if there is no league with name: leagueName
+        return retrievedLeague;
+    }
+
+    /**
+     * Adds the input league to the database
+     * @param newLeague: League object to be added to the database, the name of this league must be
+     *                 unique
+     * @throws IllegalStateException if the name of the league isn't unique
+     * @throws DatabaseException if accessing the database fails
+     */
+    public static void writeLeague(final League newLeague) throws IllegalStateException, DatabaseException{
+        // determine if league name is unique in the database, try to read in a league with this name
+        if(readLeague(newLeague.getName())!=null){
+            // there is already a league with this league's name, cannot create a league with a duplicate name
+            throw new IllegalStateException("League creation failed, league with name: " + newLeague.getName() + " already exists");
+            // TODO: could also only enforce that a league name must be unique for a sport only, not globally
+        }
+        else{
+            // this league has a unique name, write it to the database
+            database.child("Leagues").child(newLeague.getName()).setValue(newLeague);
+        }
     }
 
 
-    
+    public static void addTeam(Team newTeam){
+        // assume newTeam already has a unique name for the league it's in
+        // add team to database, this is added to Leagues/leaguename/Teams/teamname/
+        database.child("Leagues").child(newTeam.getLeague().getName()).child("Teams").child(newTeam.getName()).setValue(newTeam);
+    }
+
 }
