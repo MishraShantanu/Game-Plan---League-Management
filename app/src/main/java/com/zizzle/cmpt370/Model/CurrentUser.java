b9eb79594251eab4d16a18e86a9c6bd2be991b7a
Lec337 <lec337@mail.usask.ca>
2020-02-27 22:49:39 -0600

    Added outline of singleton pattern for the current user

--- app/src/main/java/com/zizzle/cmpt370/Model/CurrentUser.java
+++ app/src/main/java/com/zizzle/cmpt370/Model/CurrentUser.java
@@ -1,6 +1,25 @@
 package com.zizzle.cmpt370.Model;
 
+/**
+ * Uses the singleton pattern to store a Member object with information about the current user of the app
+ */
 public class CurrentUser {
-    
+    /** current user of the app */
+    private static Member currentUser = null;
+
+    private CurrentUser(){
+        // constructor is private so the class cannot be instantiated
+    }
+
+    public static Member getCurrentUser(){
+        if(currentUser == null){
+            // we don't have any info about the current user, read this from database
+            
+        }
+        // currentUser now stores info about the current user of the app
+        return currentUser;
+    }
+
+
 
 }
