8775a2d2590e6726a6fc157dc743dc89733c4eff
Lec337 <lec337@mail.usask.ca>
2020-02-28 20:35:28 -0600

    Implemented info classes

--- app/src/main/java/com/zizzle/cmpt370/Model/MemberInfo.java
+++ app/src/main/java/com/zizzle/cmpt370/Model/MemberInfo.java
@@ -1,11 +1,32 @@
 package com.zizzle.cmpt370.Model;
 
+/**
+ * Stores information about a particular team, facilitates easy database access to underlying member object
+ */
 public class MemberInfo implements InfoInterface{
+
+    /** String name of the member being represented */
+    private String memberName;
+
+    /** String key into the database where the Member represented by this object can be accessed, must be used
+     * from the correct path in the database */
+    private String memberKey;
+
+    /**
+     * Creates a MemberInfo object that stores information about the input member
+     * @param member: Member object to store information about
+     */
+    public MemberInfo(Member member){
+        memberName = member.getFirstName(); // use first names for now, could extend to first and last names
+        // a user's email must be unique, use this as a key into the database
+        memberKey = member.getEmail();
+    }
+
     public String getName(){
-        return "";
+        return memberName;
     }
 
     public String getDatabaseKey(){
-        return "";
+        return memberKey;
     }
 }
