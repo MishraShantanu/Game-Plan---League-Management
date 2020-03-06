a360a243f36419474bb262558a658f59a56bafc0
Lec337 <lec337@mail.usask.ca>
2020-03-06 03:23:06 -0600

    display name is now saved in firebase authentication profile for each user on sign up

--- app/src/main/java/com/zizzle/cmpt370/Activities/LeaguePop.java
+++ app/src/main/java/com/zizzle/cmpt370/Activities/LeaguePop.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
